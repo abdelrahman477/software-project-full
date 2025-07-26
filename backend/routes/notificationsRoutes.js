e4e357e35e5c3b19a1a08affab7163d86c0ee0be
issaabdelhaady <essa.abdelhady@gmail.com>
2025-07-26 18:52:14 +0300

    comments and notifications functionalities

--- backend/routes/notificationsRoutes.js
+++ backend/routes/notificationsRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { authenticateToken } = require('../middleware/authMiddleware');
 const notificationController = require('../controllers/notificationController');
 
-router.get('/', notificationController.getNotifications);
-router.post('/mark-read', notificationController.markAllAsRead);
+router.get('/', authenticateToken, notificationController.getNotifications);
+router.post('/mark-read', authenticateToken, notificationController.markAllAsRead);
 
 module.exports = router;
